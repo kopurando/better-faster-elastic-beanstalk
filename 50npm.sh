b2bab4eff378e3bd9e10569a2922e0857ca0c481
kopurando <664978@gmail.com>
2014-03-08 18:02:41 +0700

    better existing/installed binary detection

--- 50npm.sh
+++ 50npm.sh
@@ -11,14 +11,18 @@ npm config set fetch-retry-maxtimeout 15000
 npm config set color false
 
 #if log.io is not installed, install it and forever.js
-if [ ! -f "/usr/bin/log.io-server" ]; then
 echo "Installing forever and log.io"
+type -P log.io-server 2>&1 && echo "... found, skipping install" || {
 npm install -g forever --user 'root' &>>  /var/log/cfn-init.log
 npm install -g log.io --user 'root' &>>  /var/log/cfn-init.log
-fi
+}
+
+#install other global stuff
 echo "Installing other global NPM stuff (PhantomJS etc)"
+type -P phantomjs 2>&1 && echo "... found, skipping install" || {
 npm install -g phantomjs --user 'root' &>> /var/log/cfn-init.log
 #npm install -g casperjs --user 'root'
+}
 
 #install not-installed yet app node_modules
 if [ ! -d "/var/node_modules" ]; then
@@ -28,16 +32,15 @@ if [ -d /tmp/deployment/application ]; then
   ln -s /var/node_modules /tmp/deployment/application/
 fi
 
-#make any in-app shell scripts executable
-chmod +x /tmp/deployment/application/*.sh
-
 echo "Installing/updating NPM modules, it might take a while, go take a leak or have a healthy snack..."
 OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install &>>  /var/log/cfn-init.log) || error_exit "Failed to run npm install.  $OUT" $?
 echo $OUT
 
-echo "Logger hiccup NOW!"
-#try restarting log.io, but if log.io is not running, start it via forever
+#make any in-app shell scripts executable
+chmod +x /tmp/deployment/application/*.sh
 
+#try restarting log.io, but if log.io is not running, start it via forever
+echo "Logger hiccup NOW!"
 if [[ `pgrep -f forever` ]]; then
   /usr/bin/forever restartall
 fi
