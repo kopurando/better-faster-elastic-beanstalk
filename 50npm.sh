a65f3560dc739a2262f05a38282a0fe747e45d9e
kopurando <664978@gmail.com>
2014-03-08 20:38:25 +0700

    beauty

--- 50npm.sh
+++ 50npm.sh
@@ -10,7 +10,7 @@ function error_exit
 npm config set fetch-retry-maxtimeout 15000
 
 #if log.io is not installed, install it and forever.js
-echo "Installing forever and log.io"
+echo "------------------------------ — Installing forever and log.io — ---------------------------------------"
 type -P forever 2>&1 && echo "... found, skipping install" || {
 npm install -g forever --user 'root' &>>  /var/log/cfn-init.log
 }
@@ -19,7 +19,7 @@ npm install -g log.io --user 'root' &>>  /var/log/cfn-init.log
 }
 
 #install other global stuff
-echo "Installing other global NPM stuff (PhantomJS etc)"
+echo "------------------------------ — Installing other global NPM stuff (PhantomJS etc) — ---------------------------------------"
 type -P phantomjs 2>&1 && echo "... found, skipping install" || {
 npm install -g phantomjs --user 'root' &>> /var/log/cfn-init.log
 #npm install -g casperjs --user 'root'
@@ -33,7 +33,7 @@ if [ -d /tmp/deployment/application ]; then
   ln -s /var/node_modules /tmp/deployment/application/
 fi
 
-echo "Installing/updating NPM modules, it might take a while, go take a leak or have a healthy snack..."
+echo "------------------------------ — Installing/updating NPM modules, it might take a while, go take a leak or have a healthy snack... — ---------------------------------------" >> /var/log/cfn-init.log
 OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install &>> /var/log/cfn-init.log) || error_exit "Failed to run npm install.  $OUT" $?
 echo $OUT
 
@@ -41,7 +41,7 @@ echo $OUT
 chmod +x /tmp/deployment/application/*.sh
 
 #try restarting log.io, but if log.io is not running, start it via forever
-echo "Logger hiccup NOW!" >> /var/log/cfn-init.log
+echo "------------------------------ — Logger hiccup NOW! — ---------------------------------------" >> /var/log/cfn-init.log
 if [[ `pgrep -f forever` ]]; then
   /usr/bin/forever restartall
 fi
