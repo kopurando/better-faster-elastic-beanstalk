2f86f7efc91008badd3f896b1b809f2565ce0f51
kopurando <664978@gmail.com>
2014-03-08 12:48:55 +0700

    test

--- 50npm.sh
+++ 50npm.sh
@@ -10,8 +10,8 @@ function error_exit
 npm config set fetch-retry-maxtimeout 15000
 
 #if log.io is not installed, install it and forever.js
-echo "Installing forever & log.io"
 if [ ! -f "/usr/bin/log.io-server" ]; then
+echo "Installing forever and log.io"
 npm install -g forever --user 'root'
 npm install -g log.io --user 'root'
 fi
@@ -35,13 +35,6 @@ OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && 
 
 echo "Logger hiccup NOW!"
 #try restarting log.io, but if log.io is not running, start it via forever
-if [[ `pgrep -f forever` ]]; then
-  /usr/bin/forever restartall
-fi
-sleep 2 #make sure io-server is back up and running
-if [[ ! `pgrep -f log.io-server` ]]; then
-forever --minUptime 10000 start /usr/bin/log.io-server &> /var/log/io-server.log
-forever --minUptime 10000 start /usr/bin/log.io-harvester &> /var/log/io-harvester.log
-fi
+
 
 echo $OUT
