0d667e0a907068a70da0ed1d788a93a4ae665cd4
kopurando <664978@gmail.com>
2014-03-09 02:06:32 +0700

    ---

--- 50npm.sh
+++ 50npm.sh
@@ -10,7 +10,7 @@ function error_exit
 npm config set fetch-retry-maxtimeout 15000
 
 #if log.io is not installed, install it and forever.js
-echo "------------------------------ — Installing forever and log.io — ---------------------------------------"
+echo "------------------------------ — Installing forever and log.io — ------------------------------------" >> /var/log/cfn-init.log
 type -P forever 2>&1 && echo "... found, skipping install" || {
 npm install -g forever --user 'root' &>>  /var/log/cfn-init.log
 }
@@ -19,7 +19,7 @@ npm install -g log.io --user 'root' &>>  /var/log/cfn-init.log
 }
 
 #install other global stuff
-echo "------------------------------ — Installing other global NPM stuff (PhantomJS etc) — ---------------------------------------"
+echo "------------------------------ — Installing other global NPM stuff (PhantomJS etc) — ------------------------------------" >> /var/log/cfn-init.log
 type -P phantomjs 2>&1 && echo "... found, skipping install" || {
 npm install -g phantomjs --user 'root' &>> /var/log/cfn-init.log
 #npm install -g casperjs --user 'root'
@@ -33,7 +33,7 @@ if [ -d /tmp/deployment/application ]; then
   ln -s /var/node_modules /tmp/deployment/application/
 fi
 
-echo "------------------------------ — Installing/updating NPM modules, it might take a while, go take a leak or have a healthy snack... — ---------------------------------------" >> /var/log/cfn-init.log
+echo "------------------------------ — Installing/updating NPM modules, it might take a while, go take a leak or have a healthy snack... — -----------------------------------" >> /var/log/cfn-init.log
 OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install &>> /var/log/cfn-init.log) || error_exit "Failed to run npm install.  $OUT" $?
 echo $OUT
 
