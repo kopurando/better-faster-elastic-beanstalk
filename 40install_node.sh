5f917bd148fdf5a1d2f600945ec1f1b6e9275075
kopurando <664978@gmail.com>
2014-03-20 16:27:06 +0700

    logging

--- 40install_node.sh
+++ 40install_node.sh
@@ -12,24 +12,24 @@ function error_exit
 #rm -f /opt/elasticbeanstalk/node-install/npm_updated
 
 #download and extract desired node.js version
-echo "checking node..."
-OUT=$( [ ! -d "/opt/elasticbeanstalk/node-install" ] && echo "trying to install node.js $NODE_VER" && mkdir /opt/elasticbeanstalk/node-install ; cd /opt/elasticbeanstalk/node-install/ && \
+echo "checking node..." >> /var/log/cfn-init.log
+OUT=$( [ ! -d "/opt/elasticbeanstalk/node-install" ] && echo "trying to install node.js $NODE_VER"  >> /var/log/cfn-init.log && mkdir /opt/elasticbeanstalk/node-install ; cd /opt/elasticbeanstalk/node-install/ && \
   wget -nc http://nodejs.org/dist/v$NODE_VER/node-v$NODE_VER-linux-$ARCH.tar.gz && \
   tar --skip-old-files -xzpf node-v$NODE_VER-linux-$ARCH.tar.gz) || error_exit "Failed to UPDATE node version. $OUT" $?.
 echo $OUT
 
 #download & make install desired nginx version
-echo "checking nginx..."
-OUT=$([ ! -d "/root/nginx-$NGINX_VER" ] && echo "trying to install nginx $NGINX_VER"  && cd /root/ && curl http://nginx.org/download/nginx-$NGINX_VER.tar.gz | \
-  tar zx && cd /root/nginx-$NGINX_VER && \
+echo "checking nginx..." >> /var/log/cfn-init.log
+OUT=$([ ! -d "/root/nginx-$NGINX_VER" ] && echo "trying to install nginx $NGINX_VER"  >> /var/log/cfn-init.log && \
+ cd /root/ && curl http://nginx.org/download/nginx-$NGINX_VER.tar.gz |  tar zx && cd /root/nginx-$NGINX_VER &>> /var/log/cfn-init.log && \
   ./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log \
   --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy \
    --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi \
    --pid-path=/var/run/nginx.pid --lock-path=/var/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-ipv6 --with-http_ssl_module \
    --with-http_spdy_module --with-http_realip_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_stub_status_module \
    --with-pcre --with-google_perftools_module --with-debug  --with-ld-opt=' -Wl,-E' \
-   --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m32 -march=i686 -mtune=pentium4 -fasynchronous-unwind-tables' \
-  && make && make install)
+   --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m32 -march=i686 -mtune=pentium4 -fasynchronous-unwind-tables' &>> /var/log/cfn-init.log \
+  && make &>> /var/log/cfn-init.log && make install &>> /var/log/cfn-init.log)
 echo $OUT
 
 #make sure node binaries can be found globally
@@ -41,11 +41,11 @@ if [ ! -L /usr/bin/npm ]; then
 ln -s /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm /usr/bin/npm
 fi
 
-echo "checking npm..."
+echo "checking npm..." >> /var/log/cfn-init.log
 if [ ! -f "/opt/elasticbeanstalk/node-install/npm_updated" ]; then
 /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/ && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm update npm -g
 touch /opt/elasticbeanstalk/node-install/npm_updated
-echo "YAY! Updated global NPM version to `npm -v`"
+echo "YAY! Updated global NPM version to `npm -v`" >> /var/log/cfn-init.log
 else
-  echo "Skipping NPM -g version update. To update, please uncomment 40install_node.sh:12"
+  echo "Skipping NPM -g version update. To update, please uncomment 40install_node.sh:12" >> /var/log/cfn-init.log
 fi
