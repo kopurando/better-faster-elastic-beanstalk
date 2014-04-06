def1f710fb857f9dd1b47f194df4e543b8d27823
kopurando <664978@gmail.com>
2014-04-06 17:28:12 +0700

    moar...

--- 45phantomjs_restart.sh
+++ 45phantomjs_restart.sh
@@ -4,6 +4,5 @@ function error_exit
   eventHelper.py --msg "$1" --severity ERROR
   exit $2
 }
-OUT=$(nohup killall -USR2 phantom_monitor || cd /var/app/current/phantomjs && /usr/bin/phantom_monitor -c phantom_manager_config.yml -e development >>/var/log/phantomjs/phantom_monitor.log 2>&1 &) || error_exit "Failed to run npm install.  $OUT" $?
+OUT=$(killall -USR2 phantom_monitor || (nohup /usr/bin/phantom_monitor -c phantom_manager_config.yml -e development >>/var/log/phantomjs/phantom_monitor.log 2>&1 &) ; exit 0;) || error_exit "Failed to run npm install.  $OUT" $?
 echo $OUT
-echo "did my best"
