4f894d287b0f9dcf28f845e9bd4cf780c257dbb9
kopurando <664978@gmail.com>
2014-04-06 16:35:50 +0700

    more output for stubborn ebtools

--- 70phantomjs_restart.sh
+++ 70phantomjs_restart.sh
@@ -6,4 +6,4 @@ function error_exit
 }
 OUT=$(nohup killall -USR2 phantom_monitor || cd /var/app/current/phantomjs && /usr/bin/phantom_monitor -c phantom_manager_config.yml -e development >>/var/log/phantomjs/phantom_monitor.log 2>&1 &) || error_exit "Failed to run npm install.  $OUT" $?
 echo $OUT
-
+echo "did my best"
