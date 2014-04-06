46ba00504fd3b85c617a4ceaaf54bb889c3cdb44
kopurando <664978@gmail.com>
2014-04-06 15:57:33 +0700

    added output to avoid ebtools haning

--- 60restart_log-io.sh
+++ 60restart_log-io.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #try restarting log.io, but if log.io is not running, start it via forever
 echo "------------------------------ — Logger hiccup NOW! — ---------------------------------------" >> /var/log/cfn-init.log
 if [[ `pgrep -f forever` ]]; then
@@ -9,3 +10,4 @@ if [[ ! `pgrep -f log.io-server` ]]; then
 forever --minUptime 10000 start /usr/bin/log.io-server &> /var/log/io-server.log
 forever --minUptime 10000 start /usr/bin/log.io-harvester &> /var/log/io-harvester.log
 fi
+echo "DONE." >> /var/log/cfn-init.log
