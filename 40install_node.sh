5b5e7bcf0aed71afcf9e247141dfe5bcb841fed5
kopurando <664978@gmail.com>
2014-04-18 02:34:33 +0700

    proper log redirects via exec

--- 40install_node.sh
+++ 40install_node.sh
@@ -8,42 +8,45 @@ function error_exit
   exit $2
 }
 
+#redirect all output to cfn-init to capture it by log.io
+exec >>/var/log/cfn-init.log  2>&1
+
 #UNCOMMENT to update npm, otherwise will be updated on instance init or rebuild
 #rm -f /opt/elasticbeanstalk/node-install/npm_updated
 
 #download and extract desired node.js version
-echo "checking node..." >> /var/log/cfn-init.log
-OUT=$( [ ! -d "/opt/elasticbeanstalk/node-install" ] && echo "trying to install node.js $NODE_VER"  >> /var/log/cfn-init.log && mkdir /opt/elasticbeanstalk/node-install ; cd /opt/elasticbeanstalk/node-install/ && \
+echo "checking node..."
+OUT=$( [ ! -d "/opt/elasticbeanstalk/node-install" ] && echo "trying to install node.js $NODE_VER"   && mkdir /opt/elasticbeanstalk/node-install ; cd /opt/elasticbeanstalk/node-install/ && \
   wget -nc http://nodejs.org/dist/v$NODE_VER/node-v$NODE_VER-linux-$ARCH.tar.gz && \
   tar --skip-old-files -xzpf node-v$NODE_VER-linux-$ARCH.tar.gz) || error_exit "Failed to UPDATE node version. $OUT" $?.
 echo $OUT
 
 #download & make install desired nginx version
-echo "checking nginx..." >> /var/log/cfn-init.log
+echo "checking nginx..."
 
 #remember to add desired modules to BOTH arch-dependent commands below:
 case $( arch ) in
-( i686 ) OUT=$([ ! -d "/root/nginx-$NGINX_VER" ] && echo "trying to install nginx $NGINX_VER"  >> /var/log/cfn-init.log && \
- cd /root/ && curl http://nginx.org/download/nginx-$NGINX_VER.tar.gz |  tar zx && cd /root/nginx-$NGINX_VER &>> /var/log/cfn-init.log && \
+( i686 ) OUT=$([ ! -d "/root/nginx-$NGINX_VER" ] && echo "trying to install nginx $NGINX_VER"   && \
+ cd /root/ && curl http://nginx.org/download/nginx-$NGINX_VER.tar.gz |  tar zx && cd /root/nginx-$NGINX_VER  && \
   ./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log \
   --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy \
    --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi \
    --pid-path=/var/run/nginx.pid --lock-path=/var/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-ipv6 --with-http_ssl_module \
    --with-http_spdy_module --with-http_realip_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_stub_status_module \
    --with-pcre --with-google_perftools_module --with-debug  --with-ld-opt=' -Wl,-E' \
-   --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m32 -march=i686 -mtune=pentium4 -fasynchronous-unwind-tables' &>> /var/log/cfn-init.log \
-  && make &>> /var/log/cfn-init.log && make install &>> /var/log/cfn-init.log);;
+   --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m32 -march=i686 -mtune=pentium4 -fasynchronous-unwind-tables'  \
+  && make  && make install );;
 
-( x86_64 ) OUT=$([ ! -d "/root/nginx-$NGINX_VER" ] && echo "trying to install nginx $NGINX_VER"  >> /var/log/cfn-init.log && \
- cd /root/ && curl http://nginx.org/download/nginx-$NGINX_VER.tar.gz |  tar zx && cd /root/nginx-$NGINX_VER &>> /var/log/cfn-init.log && \
+( x86_64 ) OUT=$([ ! -d "/root/nginx-$NGINX_VER" ] && echo "trying to install nginx $NGINX_VER"   && \
+ cd /root/ && curl http://nginx.org/download/nginx-$NGINX_VER.tar.gz |  tar zx && cd /root/nginx-$NGINX_VER  && \
   ./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log \
   --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy \
    --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi \
    --pid-path=/var/run/nginx.pid --lock-path=/var/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-ipv6 --with-http_ssl_module \
    --with-http_spdy_module --with-http_realip_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_stub_status_module \
    --with-pcre --with-google_perftools_module --with-debug \
-   --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic' --with-ld-opt=' -Wl,-E' &>> /var/log/cfn-init.log \
-  && make &>> /var/log/cfn-init.log && make install &>> /var/log/cfn-init.log);;
+   --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic' --with-ld-opt=' -Wl,-E'  \
+  && make  && make install );;
 esac
 
 echo $OUT
@@ -57,11 +60,11 @@ if [ ! -L /usr/bin/npm ]; then
 ln -s /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm /usr/bin/npm
 fi
 
-echo "checking npm..." >> /var/log/cfn-init.log
+echo "checking npm..."
 if [ ! -f "/opt/elasticbeanstalk/node-install/npm_updated" ]; then
 cd /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/ && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm update npm -g
 touch /opt/elasticbeanstalk/node-install/npm_updated
-echo "YAY! Updated global NPM version to `npm -v`" >> /var/log/cfn-init.log
+echo "YAY! Updated global NPM version to `npm -v`"
 else
-  echo "Skipping NPM -g version update. To update, please uncomment 40install_node.sh:12" >> /var/log/cfn-init.log
+  echo "Skipping NPM -g version update. To update, please uncomment 40install_node.sh:12"
 fi
--- 50npm.sh
+++ 50npm.sh
@@ -6,23 +6,23 @@ function error_exit
   exit $2
 }
 
+#redirect all output to cfn-init to capture it by log.io
+exec >>/var/log/cfn-init.log  2>&1
 #avoid long NPM fetch hangups
 npm config set fetch-retry-maxtimeout 15000
-#redirect all output to cfn-init to capture it by log.io
-exec  2>&1
-#if log.io is not installed, install it and forever.js and require.js
-echo "------------------------------ — Installing forever, requirejs and log.io — ------------------------------------"
-type -P forever  && echo "... found, skipping install"  || npm install -g --production forever --user 'root' &
-type -P log.io-server  && echo "... found, skipping install"   || npm install -g --production log.io --user 'root' &
-type -P r.js  && echo "... found, skipping install"   || npm install -g --production requirejs@">=2.1.11 <3.0.0" --user 'root' &
-type -P jade  && echo "... found, skipping install"   || npm install -g --production jade@">=1.3.1 <2.0.0" --user 'root' &
+#if log.io is not installed, install it and forever.js
+echo "------------------------------ — Installing forever and log.io — ------------------------------------"
+type -P forever  && echo "... found, skipping install"  || npm install -g --production forever --user 'root'
+type -P log.io-server  && echo "... found, skipping install"   || npm install -g --production log.io --user 'root'
 
 #install other global stuff
-echo "------------------------------ — Installing other global NPM stuff (PhantomJS etc) — ------------------------------------"
+echo "------------------------------ — Installing other global NPM stuff (PhantomJS, RequireJS, jade etc) — ------------------------------------"
 type -P phantomjs  && echo "... found, skipping install"  || {
-npm install -g --production phantomjs@">=1.9.6 <2.0.0" --user 'root' &
+npm install -g --production phantomjs@">=1.9.6 <2.0.0" --user 'root'
 #npm install -g --production casperjs --user 'root'
 }
+type -P r.js  && echo "... found, skipping install"   || npm install -g --production requirejs@">=2.1.11 <3.0.0" --user 'root'
+type -P jade  && echo "... found, skipping install"   || npm install -g --production jade@">=1.3.1 <2.0.0" --user 'root'
 
 #install not-installed yet app node_modules
 if [ ! -d "/var/node_modules" ]; then
@@ -37,7 +37,7 @@ IO_LOG_NODE=`grep IO_LOG_NODE /etc/init/nodejs.conf | cut --delimiter='"' --fiel
 fi
 
 echo "------------------------------ — Installing/updating NPM modules, it might take a while, go take a leak or have a healthy snack... — -----------------------------------"
-OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install --production &) || error_exit "Failed to run npm install.  $OUT" $?
+OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install --production) || error_exit "Failed to run npm install.  $OUT" $?
 echo $OUT
 
 #try restarting log.io, but if log.io is not running, start it via forever
