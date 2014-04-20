072ecff483f92c3e7335d16859c6ccae8d40d7f4
kopurando <664978@gmail.com>
2014-04-20 21:11:36 +0700

    success

--- 10stop.sh
+++ 10stop.sh
@@ -6,9 +6,6 @@ function error_exit
   exit $2
 }
 
-#configs can't be moved from enf.conf as ebnode.py overwrites dir after YML has been processed
-#mv -vf /tmp/deployment/config/#etc#nginx#conf.d#00_elastic_beanstalk_proxy.conf /etc/nginx/conf.d/00_elastic_beanstalk_proxy.conf >> /var/log/cfn-init.log
-#mv -vf /opt/elasticbeanstalk/*.snippet /etc/nginx/conf.d/ >> /var/log/cfn-init.log
 echo "Testing nginx config.... " >> /var/log/cfn-init.log
 #test nginx config before proceeding with restart
 OUT=$(/usr/sbin/nginx -tc /etc/nginx/nginx.conf && /opt/elasticbeanstalk/containerfiles/ebnode.py --action stop-all 2>&1) || error_exit "Failed to stop service daemons, CHECK NGINX CONFIG $OUT" $?
