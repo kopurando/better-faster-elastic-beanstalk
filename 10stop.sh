8403aa301be52b3518bd0d3f1e2d3f36ffcc7e33
kopurando <664978@gmail.com>
2014-04-20 17:44:23 +0700

    comments

--- 10stop.sh
+++ 10stop.sh
@@ -5,7 +5,7 @@ function error_exit
   eventHelper.py --msg "$1" --severity ERROR
   exit $2
 }
-
+echo "Testing nginx config.... " >> /var/log/cfn-init.log
 #test nginx config before proceeding with restart
-OUT=$(/usr/sbin/nginx -tc /etc/nginx/nginx.conf && /opt/elasticbeanstalk/containerfiles/ebnode.py --action stop-all 2>&1) || error_exit "Failed to stop service daemons. $OUT" $?
+OUT=$(/usr/sbin/nginx -tc /etc/nginx/nginx.conf && /opt/elasticbeanstalk/containerfiles/ebnode.py --action stop-all 2>&1) || error_exit "Failed to stop service daemons, CHECK NGINX CONFIG $OUT" $?
 echo $OUT
