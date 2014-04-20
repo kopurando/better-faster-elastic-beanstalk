ae72a1adf774251688b4d8ecded16df290675f0e
kopurando <664978@gmail.com>
2014-04-20 21:16:39 +0700

    again!

--- 10stop.sh
+++ 10stop.sh
@@ -6,6 +6,8 @@ function error_exit
   exit $2
 }
 
+#in order to test config should be moved at this stage as ebnode.py overwrites config after YML has been processed and that can lead to nginx start failure
+mv -vf /tmp/deployment/config/#etc#nginx#conf.d#00_elastic_beanstalk_proxy.conf /etc/nginx/conf.d/00_elastic_beanstalk_proxy.conf >> /var/log/cfn-init.log
 echo "Testing nginx config.... " >> /var/log/cfn-init.log
 #test nginx config before proceeding with restart
 OUT=$(/usr/sbin/nginx -tc /etc/nginx/nginx.conf && /opt/elasticbeanstalk/containerfiles/ebnode.py --action stop-all 2>&1) || error_exit "Failed to stop service daemons, CHECK NGINX CONFIG $OUT" $?
