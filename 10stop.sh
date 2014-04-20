819708e748c9f7810613c2f13fc1ee7dc5cd1ec3
kopurando <664978@gmail.com>
2014-04-20 18:48:52 +0700

    move snippets to nginx conf dir

--- 10stop.sh
+++ 10stop.sh
@@ -5,6 +5,8 @@ function error_exit
   eventHelper.py --msg "$1" --severity ERROR
   exit $2
 }
+
+mv -v /opt/elasticbeanstalk/*.snippet /etc/nginx/conf.d/ >> /var/log/cfn-init.log
 echo "Testing nginx config.... " >> /var/log/cfn-init.log
 #test nginx config before proceeding with restart
 OUT=$(/usr/sbin/nginx -tc /etc/nginx/nginx.conf && /opt/elasticbeanstalk/containerfiles/ebnode.py --action stop-all 2>&1) || error_exit "Failed to stop service daemons, CHECK NGINX CONFIG $OUT" $?
