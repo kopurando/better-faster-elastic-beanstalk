6028dd28719c5a85f921324bc3ed42ab2ad6756c
kopurando <664978@gmail.com>
2014-05-17 10:39:38 +0700

    46update_geolitecity.sh to fetch/update GeoLiteCity.dat

---
+++ 46update_geolitecity.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+ function error_exit
+ {
+   eventHelper.py --msg "$1" --severity ERROR
+   exit $2
+ }
+ exec >>/var/log/cfn-init.log  2>&1
+ [ ! -f "/var/GeoLiteCity.dat" ] && echo "GeoLiteCity.dat not found, fetching new one..."  && cd /var/ && curl http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz | zcat > /var/GeoLiteCity.new && mv -vf /var/GeoLiteCity.new /var/GeoLiteCity.dat
+
+ OUT=$(cp -vf /var/GeoLiteCity.dat /var/app/current/geolite_data/GeoLiteCity.dat) || error_exit "Failed to fetch/1update GeoLiteCity.dat  $OUT" $?
+ echo $OUT
