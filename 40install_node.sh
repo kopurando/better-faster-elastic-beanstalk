6617bede99530a252bb45e777898b34e2c081973
kopurando <664978@gmail.com>
2014-05-17 19:44:16 +0700

    compute md5 for js/lib folder as well

--- 40install_node.sh
+++ 40install_node.sh
@@ -69,7 +69,7 @@ echo "running r.js build....."
 OUT=$(cd /tmp/deployment/application/public && /usr/bin/r.js -o build.js >> /var/log/cfn-init.log && mv -v /tmp/deployment/application/public/dist /tmp/deployment/application/dist && rm -rf /tmp/deployment/application/public/ && mv -v /tmp/deployment/application/dist /tmp/deployment/application/public ) || error_exit "Failed to run r.js optimizer. $OUT" $?
 echo $OUT
 echo "computing md5 hashes...."
-cd /tmp/deployment/application/public/ && find . -maxdepth 2 -type f  -iname '*js' -o -iname '*css' -type f | xargs  md5sum | awk '{system("echo "$1" > "$2".md5")}'
+cd /tmp/deployment/application/public/ && find . -maxdepth 3 -type f  -iname '*js' -o -iname '*css' -type f | xargs  md5sum | awk '{system("echo "$1" > "$2".md5")}'
 echo "gzipping everything!"
 cd /tmp/deployment/application/public/ && find . -type f -iname "*css" -o -iname "*js" -type f  | while read -r x;do   gzip -9 -c "$x" > "$x.gz";done
 echo "touching all files to make them dated with the same time (as per nginx gzip_static recommendation)"
