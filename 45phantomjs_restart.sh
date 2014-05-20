9a4f6a11154c03bc7bf1966d9d28b741ec7b7ef1
kopurando <664978@gmail.com>
2014-05-20 23:25:15 +0700

    inject IO_LOG_NODE var

--- 45phantomjs_restart.sh
+++ 45phantomjs_restart.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #if instance is not suited for phantom, dont run it
-if [[ $IO_LOG_NODE != *PHANTO* ]] ; then  exit 0 ; fi ;
+
+if [ -f "/etc/init/nodejs.conf" ]; then
+IO_LOG_NODE=`grep IO_LOG_NODE /etc/init/nodejs.conf | cut --delimiter='"' --fields=2`
+fi
+
+if [[ $IO_LOG_NODE != *PHANTO* ]] ; then echo "Not a PHANTOMJS intsance, doing nothing, bye." exit 0 ; fi ;
 function error_exit
 {
   eventHelper.py --msg "$1" --severity ERROR
