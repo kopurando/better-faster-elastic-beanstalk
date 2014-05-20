48caba9ecf1c28779f25eba69121359cfdf852ab
kopurando <664978@gmail.com>
2014-05-20 20:35:56 +0700

    update path to phmon

--- 45phantomjs_restart.sh
+++ 45phantomjs_restart.sh
@@ -4,5 +4,5 @@ function error_exit
   eventHelper.py --msg "$1" --severity ERROR
   exit $2
 }
-OUT=$(/var/app/current/phmon_restart.sh) || error_exit "Failed to restart PhantomJS.  $OUT" $?
+OUT=$(/var/app/current/phantomjs/phmon_restart.sh) || error_exit "Failed to restart PhantomJS.  $OUT" $?
 echo $OUT
