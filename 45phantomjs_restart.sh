37976aeaa3781acad48c86a1a239f95ffcfb1d9b
kopurando <664978@gmail.com>
2014-05-21 00:09:54 +0700

    typo

--- 45phantomjs_restart.sh
+++ 45phantomjs_restart.sh
@@ -6,7 +6,7 @@ if [ -f "/etc/init/nodejs.conf" ]; then
 IO_LOG_NODE=`grep IO_LOG_NODE /etc/init/nodejs.conf | cut --delimiter='"' --fields=2`
 fi
 
-if [[ $IO_LOG_NODE != *PHANTO* ]] ; then echo "Not a PHANTOMJS intsance, doing nothing, bye." exit 0 ; fi ;
+if [[ $IO_LOG_NODE != *PHANTO* ]] ; then echo "Not a PHANTOMJS intsance, doing nothing, bye." && exit 0 ; fi ;
 function error_exit
 {
   eventHelper.py --msg "$1" --severity ERROR
