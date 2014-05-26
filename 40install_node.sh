adf18e022fe4bbdbbd98c1f608b9b1f9465498c2
kopurando <664978@gmail.com>
2014-05-26 21:46:27 +0700

    undo

--- 40install_node.sh
+++ 40install_node.sh
@@ -10,13 +10,6 @@ function error_exit
 
 #redirect all output to cfn-init to capture it by log.io
 exec >>/var/log/cfn-init.log  2>&1
-#download and extract desired node.js version
-echo "checking node..."
-OUT=$( [ ! -d "/opt/elasticbeanstalk/node-install" ] && echo "trying to install node.js $NODE_VER"   && mkdir /opt/elasticbeanstalk/node-install ; cd /opt/elasticbeanstalk/node-install/ && \
-  wget -nc http://nodejs.org/dist/v$NODE_VER/node-v$NODE_VER-linux-$ARCH.tar.gz && \
-  tar --skip-old-files -xzpf node-v$NODE_VER-linux-$ARCH.tar.gz) || error_exit "Failed to UPDATE node version. $OUT" $?.
-echo $OUT
-
 
 #UNCOMMENT to update npm, otherwise will be updated on instance init or rebuild
 #rm -f /opt/elasticbeanstalk/node-install/npm_updated
