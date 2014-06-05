91fd9e8c0a4f7e65ec45a7eb533fdd5bd67ec096
kopurando <664978@gmail.com>
2014-06-05 16:14:23 +0700

    IO_LOG_NODE = env. name to support multiple envs per app

--- 55phantomjs_restart.sh
+++ 55phantomjs_restart.sh
@@ -6,12 +6,11 @@ if [ -f "/etc/init/nodejs.conf" ]; then
 IO_LOG_NODE=`grep IO_LOG_NODE /etc/init/nodejs.conf | cut --delimiter='"' --fields=2`
 fi
 
-if [[ $IO_LOG_NODE != *PHANTO* ]] ; then echo "Not a PHANTOMJS intsance, doing nothing, bye." && exit 0 ; fi ;
+if [[ $IO_LOG_NODE != *phanto* ]] ; then echo "Not a PHANTOMJS intsance, doing nothing, bye." && exit 0 ; fi ;
 function error_exit
 {
   eventHelper.py --msg "$1" --severity ERROR
   exit $2
 }
-echo "Restarting phantomjs heads..."
 OUT=$(/var/app/current/phantomjs/phmon_restart.sh) || error_exit "Failed to restart PhantomJS.  $OUT" $?
 echo $OUT
